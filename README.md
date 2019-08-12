36256747eb056c2bc157e0cab58b340aeac10201
758C93E6D59E83352B950F202FD92EBC <758C93E6D59E83352B950F202FD92EBC@i-search.com.cn>
2019-08-12 13:26:26 +0800

    更改文件目录

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Java-leaning
+﻿# Java-leaning
 这是我java学习代码
+前三天均为非idea项目后面项目均在idea上开发
+
--- day04/day04-code/src/cn/isearch/day04/demo01/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package cn.isearch.day04.demo01;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
