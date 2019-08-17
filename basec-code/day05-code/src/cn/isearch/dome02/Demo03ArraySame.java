102232f3569067a7356ba5d899e2aa4aa4bb168d
758C93E6D59E83352B950F202FD92EBC <758C93E6D59E83352B950F202FD92EBC@i-search.com.cn>
2019-08-17 22:58:11 +0800

    新增demo

---
+++ basec-code/day05-code/src/cn/isearch/dome02/Demo03ArraySame.java
@@ -0,0 +1,31 @@
+package cn.isearch.dome02;
+
+public class Demo03ArraySame {
+    public static void main(String[] args) {
+        int[] arrayA = new int[3];
+        System.out.println(arrayA);
+        System.out.println(arrayA[0]);
+        System.out.println(arrayA[1]);
+        System.out.println(arrayA[2]);
+        System.out.println("==============");
+        arrayA[1] = 10;
+        arrayA[2] = 20;
+        System.out.println(arrayA);
+        System.out.println(arrayA[0]);
+        System.out.println(arrayA[1]);
+        System.out.println(arrayA[2]);
+
+        int[] ArrayB = arrayA;
+        System.out.println(ArrayB);
+        System.out.println(ArrayB[0]);
+        System.out.println(ArrayB[1]);
+        System.out.println(ArrayB[2]);
+        System.out.println("==============");
+        ArrayB[1] = 100;
+        ArrayB[2] = 200;
+        System.out.println(ArrayB);  // 内存地址值
+        System.out.println(ArrayB[0]);  // 0
+        System.out.println(ArrayB[1]);  // 10
+        System.out.println(ArrayB[2]);  // 20
+    }
+}
